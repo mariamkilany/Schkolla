b816f9e219b74500ef4a1afa0a3500b56a440cfd
FatmaFawzy <fatmabata133@gmail.com>
2023-01-31 19:41:58 +0200

    rgister component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Slider from './components/studentSlider/Slider';
+import Register from './components/studentSlider/Register/Register';
+
 function App() {
   return (
     <div className="App">
       {/* <Slider/> */}
+      <Register/>
     </div>
   );
 }
