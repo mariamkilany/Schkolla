50a8e123f89be394dd1307a3c2a9bb8cb43ba7c2
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-01-31 18:48:44 +0200

    changes

--- src/components/studentSlider/Card.js
+++ src/components/studentSlider/Card.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-function StuCard() {
+import { RiArrowRightSLine , RiArrowLeftSLine} from 'react-icons/ri'
+import Shift from './Shift';
+function StuCard(stu) {
+    const student =stu.props
     return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem', display: student.display==='curr'||student.display==='prev'||student.display==='next'?'block':'none' }} 
+                className={`col-4 ${student.display==='curr'?'order-2':''}
+                ${student.display==='prev'?'order-1':''}
+                ${student.display==='prev'?'order-3':''}`}>
         <Card.Img variant="top" src="./images/student1.png" />
         <Card.Body>
-        <Card.Title>Card Title</Card.Title>
         <Card.Text>
-            
+            <span>الإسم: {student.name}</span>
+            <span>المرحلة: {student.level}</span>
+            <span> الفصل: {student.class}</span>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button className='btn btn-secondary'>عرض الطالب</Button>
+        <Shift/>
         </Card.Body>
     </Card>
     );
--- src/components/studentSlider/Slider.js
+++ src/components/studentSlider/Slider.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StuCard from './Card'
 import './slider.css';
 import staudents from './data'
 
+const handleCurrent = (num)=>{
+    if(num<0){
+        return staudents.length -1
+    }
+    else if(num===staudents.length)
+        return 0
+    else
+    return num
+}
+
 function Slider() {
     console.log(staudents)
+    const [current , setCurrent]=useState(0);
     return (
-    <div>
-        {staudents.map((student)=>{
-            return <StuCard student key={student.id} />
+    <div className='row'>
+        {staudents.map((student,index)=>{
+            console.log(index)
+            if(index===current)
+            return <StuCard props={{...student,display:'curr'}} key={student.id} />
+            else if(index===handleCurrent(current-1)){
+            return <StuCard props={{...student,display:'prev'}} key={student.id} />
+            }
+            else if(index===handleCurrent(current+1))
+            return <StuCard props={{...student,display:'next'}} key={student.id} />
+            else 
+            return <StuCard props={student} key={student.id} />
         })}
     </div>
     )
