a276e211afed19f9eced7482e57c6086ab32ce05
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-02-01 03:42:42 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import Register from './components/studentSlider/Register/Register';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <Slider/> */}
       <Register/>
-=======
       <Slider/>
->>>>>>> 50a8e123f89be394dd1307a3c2a9bb8cb43ba7c2
     </div>
   );
 }
