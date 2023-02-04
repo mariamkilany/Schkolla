af8f467fbaf87d35fd685b02829a1ac08b16b2da
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-02-04 22:25:51 +0200

    msg

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, {useState } from 'react'
 import './rgister.css'
+import axios from 'axios'
 
-export default function register() {
+export default function Register() {
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const msg=await axios.post(`https://erin-hungry-bonobo.cyclic.app/v1/admin/login`,{email,password})
+    console.log(msg)
+  }
   return (
-  <div>
-        <div className='log-container col-3'>
-            <h2>أهلا بعودتك</h2>
-            <div className="input-cont">
-                <input type="text" placeholder='أسم المستخدم' />
-                <input type="password" placeholder='كلمة المرور' />
-            </div>
-            <button type="submit" className="btn btn-primary">تسجيل الدخول</button>
-        </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+    <div>
+          <div className='log-container col-3'>
+              <h2>أهلا بعودتك</h2>
+              <div className="input-cont">
+                  <input type="text" placeholder='أسم المستخدم' onChange={(e)=>setEmail(e.target.value)} />
+                  <input type="password" placeholder='كلمة المرور' onChange={(e)=>setPassword(e.target.value)} />
+              </div>
+              <button type="submit" className="btn btn-primary">تسجيل الدخول</button>
+          </div>
+      </div>
+    </form>
   )
 }
