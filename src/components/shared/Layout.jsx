748ac4f18d8330d64c44d02b5535fd66322c7b5b
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-02-14 17:32:38 +0200

    mor info teacher

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -12,15 +12,15 @@ const Layout = ({ children }) => {
         <>
         {!(location.pathname.toLowerCase().includes('dashboard'))&&
         <Navbar style={{background:'#F3F4F8'}}>
-            <Navbar.Brand as={Link} to="/">
-                    <img src={logo} alt=""  className="w-50"/>
+            <Navbar.Brand as={Link} to="/" className="d-flex">
+                    <img src={logo} alt="logo"  className="w-50"/>
             </Navbar.Brand>
-            <Nav className="ms-auto me-5 ">
+            <Nav className="ms-auto me-2 ">
                 {accessToken && (<Nav.Link as={Link} to="/dashboard">لوحة التحكم</Nav.Link>)}
             </Nav>
             {!accessToken&&location.pathname!=='/login' &&
-            <Nav className="ms-auto me-5 ">
-           <Nav.Link as={Link} to="/login">تسجيل الدخول</Nav.Link>
+            <Nav className="ms-auto me-2">
+            <Nav.Link as={Link} to="/login">تسجيل الدخول</Nav.Link>
             </Nav>}
         </Navbar>}
         <Container>{children}</Container>
